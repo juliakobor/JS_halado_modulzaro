05d4c54fc9a504ccf67e042173046eaa05f8704f
Kóbor Júlia <Kbor Jlia>
2021-03-06 11:46:19 +0100

    post and user classes

--- OOP/post.js
+++ OOP/post.js
@@ -2,10 +2,14 @@ export class Post {
     title;
     body;
     id;
+    getPreview(length);
     constructor(title, body, id) {
-        this.title=title;
-        this.body=body;
-        this.id=id;
+        this.title = title;
+        this.body = body;
+        this.id = id;
+        this.getPreview = function() {
+            return this.body
+        }
     }
 }
 
--- OOP/user.js
+++ OOP/user.js
@@ -1,3 +1,16 @@
 export class User {
-
+    firstName;
+    lastName;
+    email;
+    constructor(firstName, lastName, email) {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+    }
+    get fullname(){
+        return this.firstName + this.lastName;
+    }
+    get stringEmail(){
+        return this.email.toString();
+    }
 }
