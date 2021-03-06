8f893dab254927025aafb19d8319fbf096cc1d57
Kóbor Júlia <Kbor Jlia>
2021-03-06 16:55:03 +0100

    reformat code

--- OOP/blog.js
+++ OOP/blog.js
@@ -6,48 +6,47 @@ export class Blog {
     constructor(title, author) {
         this.#title = title;
         this.#author = author;
-    }
 
-    get Posts() {
-        return this.#posts;
-    }
-
-    fetchPosts = function () {
-        let fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
-        fetchPromise
-            .then(response => {
-                if (!response.ok) throw Error(response.statusText)
-                else return response.json()
-            })
-            .then(data => {
-                data.forEach(item => {
-                    this.#posts.push(item)
-                    //this.#posts.push(new Post(item.id, item.body, item.id))
+        this.fetchPosts = function () {
+            let fetchPromise = fetch("https://jsonplaceholder.typicode.com/posts");
+            fetchPromise
+                .then(response => {
+                    if (!response.ok) throw Error(response.statusText)
+                    else return response.json()
                 })
-            })
-            .catch(error => {
-                console.log("error:", error)
-            });
-    }
-    addPost = function (post) {
-        this.#posts.push(post);
-    }
+                .then(data => {
+                    data.forEach(item => {
+                        this.#posts.push(item)
+                        //this.#posts.push(new Post(item.id, item.body, item.id))
+                    })
+                })
+                .catch(error => {
+                    console.log("error:", error)
+                });
+        }
+        this.addPost = function (post) {
+            this.#posts.push(post);
+        }
 
-    deletePost = function (id) {
-        for (let i = 0; i < this.#posts.length; i++) {
-            if (id === this.#posts[i].id) {
-                return this.#posts.splice(i, 1)
+        this.deletePost = function (id) {
+            for (let i = 0; i < this.#posts.length; i++) {
+                if (id === this.#posts[i].id) {
+                    return this.#posts.splice(i, 1)
+                }
+            }
+        }
+
+        this.getPostById = function (id) {
+            for (let i = 0; i < this.#posts.length; i++) {
+                if (id === this.#posts[i].id) {
+                    console.log(this.#posts[i]);
+                } else return null;
             }
         }
     }
 
-
-    getPostById = function (id) {
-        for (let i = 0; i < this.#posts.length; i++) {
-            if (id === this.#posts[i].id) {
-                console.log(this.#posts[i]);
-            } else return null;
-        }
+    get Posts() {
+        return this.#posts;
     }
 }
 
